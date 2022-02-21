ff07d603ae2e69995c8fbb92fff442669d343b75
m4st3rbyt3 <joseajcip@outlook.es>
2022-02-21 10:29:34 -0600

    initial commit

---
+++ Digifact/Controllers/InputController.cs
@@ -0,0 +1,22 @@
+﻿using Digifact.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Digifact.Controllers
+{
+    public class InputController : ApiController
+    {
+        Input dataIn = new Input();
+
+        [HttpPost]
+        public string PostConversion(Input buffer)
+        {
+            //Console.Write(buffer.Data);
+            return buffer.Data;
+        }
+    }
+}
---
+++ Digifact/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Digifact.WebApiApplication" Language="C#" %>
